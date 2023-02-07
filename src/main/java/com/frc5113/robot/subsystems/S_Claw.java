f20441c3b8d8df62a94d29116c409d4e315e7ec6
Vlad Bondar <33793511+drvladb@users.noreply.github.com>
2023-02-07 02:03:37 +0000

    broken claw systems

---
+++ src/main/java/com/frc5113/robot/subsystems/S_Claw.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.frc5113.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class S_Claw extends SubsystemBase {
+  /** Creates a new S_Claw. */
+
+  public final DoubleSolenoid clawSolenoid;
+   
+  public S_Claw() {
+    clawSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/com/frc5113/robot/subsystems/S_Pneumatics.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.frc5113.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class S_Pneumatics extends SubsystemBase {
+  /** Creates a new S_Pneumatics. */
+  public S_Pneumatics() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
