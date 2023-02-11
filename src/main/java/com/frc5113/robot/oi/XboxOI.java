132ee289406f37cebd2c6a301bd25ae5c7789130
Vlad Bondar <vlad@vladb.xyz>
2023-02-11 07:55:48 -0500

    Add OperatorInterface Interface & impls

---
+++ src/main/java/com/frc5113/robot/oi/XboxOI.java
@@ -0,0 +1,42 @@
+package com.frc5113.robot.oi;
+
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+import java.util.function.Supplier;
+
+public class XboxOI implements IOI {
+  private final CommandXboxController xbox = new CommandXboxController(0);
+
+  /**
+   * @return Axis responsible for arcade speed
+   */
+  @Override
+  public Supplier<Double> arcadeSpeed() {
+    return xbox::getLeftY;
+  }
+
+  /**
+   * @return Axis responsible for arcade turn
+   */
+  @Override
+  public Supplier<Double> arcadeTurn() {
+    return xbox::getRightX;
+  }
+
+  /**
+   * @return Axis responsible for tank left side
+   */
+  @Override
+  public Supplier<Double> tankL() {
+    return xbox::getLeftY;
+  }
+
+  /**
+   * @return Axis responsible for tank right side
+   */
+  @Override
+  public Supplier<Double> tankR() {
+    return xbox::getRightY;
+  }
+}
