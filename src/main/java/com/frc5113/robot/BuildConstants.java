56d5cc36e626a88fe251aa9b41ca1327a75878b8
Vlad Bondar <vlad@vladb.xyz>
2023-02-14 13:22:34 -0500

    replace build constants with empty text

--- src/main/java/com/frc5113/robot/BuildConstants.java
+++ src/main/java/com/frc5113/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package com.frc5113.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2023-Pandemonium";
-  public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "8568a35c287045bc910d91ebd4689d6b913aa4d8";
-  public static final String GIT_DATE = "2023-02-14 13:19:54 EST";
-  public static final String GIT_BRANCH = "vb-logging";
-  public static final String BUILD_DATE = "2023-02-14 13:21:21 EST";
-  public static final long BUILD_UNIX_TIME = 1676398881278L;
-  public static final int DIRTY = 1;
+  public static final String MAVEN_NAME = "";
+  public static final String VERSION = "";
+  public static final int GIT_REVISION = 0;
+  public static final String GIT_SHA = "";
+  public static final String GIT_DATE = "";
+  public static final String GIT_BRANCH = "";
+  public static final String BUILD_DATE = "";
+  public static final long BUILD_UNIX_TIME = 0L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
