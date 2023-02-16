4b7eb1fb646085490657577212c724b1ffb0b602
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2023-02-16 15:25:15 -0500

    add auto balance

--- src/main/java/com/frc5113/robot/constants/DrivetrainConstants.java
+++ src/main/java/com/frc5113/robot/constants/DrivetrainConstants.java
@@ -8,5 +8,16 @@ public class DrivetrainConstants {
   public static final int RIGHT_LEADER_ID = 11;
   public static final int RIGHT_FOLLOWER_ID = 21;
 
+  // Odometry stuff
+  public static final double TRACK_WIDTH = 0.; // In inches
+  public static final double WHEEL_CIRCUMFERENCE = 0.; // In inches
+  public static final double GEAR_RATIO = 0.;
   public static final IdleMode MOTOR_MODE = IdleMode.kCoast;
+
+  // Auto Balance stuff
+  public static final double BALANCE_KP = 0.;
+  public static final double BALANCE_KI = 0.;
+  public static final double BALANCE_KD = 0.;
+  public static final double BALANCE_TOLERANCE_P = 0.;
+  public static final double BALANCE_TOLERANCE_D = 0.;
 }
