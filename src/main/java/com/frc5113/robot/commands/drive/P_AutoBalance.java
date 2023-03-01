5740902246ebd646abe1c85a7f94bce670079d6e
jrego05 <jregovich05@gmail.com>
2023-03-01 12:06:14 -0800

    rebase commit no5

--- src/main/java/com/frc5113/robot/commands/drive/P_AutoBalance.java
+++ src/main/java/com/frc5113/robot/commands/drive/P_AutoBalance.java
@@ -11,12 +11,20 @@ public class P_AutoBalance extends PIDCommand {
 
   public P_AutoBalance(S_DriveTrain driveTrain, S_Gyro gyro) {
     super(
+<<<<<<< HEAD
         new PIDController(BALANCE_KP.get(), BALANCE_KI.get(), BALANCE_KD.get()),
+=======
+        new PIDController(BALANCE_KP, BALANCE_KI, BALANCE_KD),
+>>>>>>> 41436d7 (add auto balance)
         () -> gyro.getRoll(),
         0.0,
         output -> driveTrain.tankDrive(output, output),
         driveTrain);
+<<<<<<< HEAD
     getController().setTolerance(BALANCE_TOLERANCE_P.get(), BALANCE_TOLERANCE_D.get());
+=======
+    getController().setTolerance(BALANCE_TOLERANCE_P, BALANCE_TOLERANCE_D);
+>>>>>>> 41436d7 (add auto balance)
   }
 
   @Override
--- src/main/java/com/frc5113/robot/constants/DrivetrainConstants.java
+++ src/main/java/com/frc5113/robot/constants/DrivetrainConstants.java
@@ -20,4 +20,16 @@ public class DrivetrainConstants {
   public static final int RIGHT_FOLLOWER_ID_PANDEGUARDIUM = 22;
 
   public static final NeutralMode MOTOR_MODE_PANDEGUARDIUM = NeutralMode.Brake;
+  // Odometry stuff
+  public static final double TRACK_WIDTH = 0.; // In inches
+  public static final double WHEEL_CIRCUMFERENCE = 0.; // In inches
+  public static final double GEAR_RATIO = 0.;
+  public static final IdleMode MOTOR_MODE = IdleMode.kCoast;
+
+  // Auto Balance stuff
+  public static final double BALANCE_KP = 0.;
+  public static final double BALANCE_KI = 0.;
+  public static final double BALANCE_KD = 0.;
+  public static final double BALANCE_TOLERANCE_P = 0.;
+  public static final double BALANCE_TOLERANCE_D = 0.;
 }
