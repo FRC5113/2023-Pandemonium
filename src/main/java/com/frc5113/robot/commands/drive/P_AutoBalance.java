02a4a7fe6f90f75c30c46f1b2e167f047ed27f82
jrego05 <jregovich05@gmail.com>
2023-03-01 12:10:59 -0800

    auto charging drivetrain fix

--- src/main/java/com/frc5113/robot/commands/drive/P_AutoBalance.java
+++ src/main/java/com/frc5113/robot/commands/drive/P_AutoBalance.java
@@ -4,7 +4,7 @@ import static com.frc5113.robot.constants.DrivetrainConstants.*;
 import static com.frc5113.robot.constants.GeneralConstants.TUNING_MODE;
 
 import com.frc5113.library.utils.tunable.TunableNumber;
-import com.frc5113.robot.subsystems.S_DriveTrain;
+import com.frc5113.robot.subsystems.DriveTrain;
 import com.frc5113.robot.subsystems.S_Gyro;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
@@ -19,38 +19,14 @@ public class P_AutoBalance extends PIDCommand {
   public static final TunableNumber BALANCE_TOLERANCE_D =
       new TunableNumber("Arm: Velocity Tolerance", 1, TUNING_MODE);
 
-  public P_AutoBalance(S_DriveTrain driveTrain, S_Gyro gyro) {
+  public P_AutoBalance(DriveTrain driveTrain, S_Gyro gyro) {
     super(
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         new PIDController(BALANCE_KP.get(), BALANCE_KI.get(), BALANCE_KD.get()),
-=======
-        new PIDController(BALANCE_KP, BALANCE_KI, BALANCE_KD),
->>>>>>> 41436d7 (add auto balance)
-=======
-        new PIDController(BALANCE_KP.get(), BALANCE_KI.get(), BALANCE_KD.get()),
->>>>>>> 88faa89 (implemented tunable numbers)
-=======
-        new PIDController(BALANCE_KP.get(), BALANCE_KI.get(), BALANCE_KD.get()),
->>>>>>> fef06d234b1f4d0de6df11bb76090c722c603648
         () -> gyro.getRoll(),
         0.0,
         output -> driveTrain.tankDrive(output, output),
         driveTrain);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     getController().setTolerance(BALANCE_TOLERANCE_P.get(), BALANCE_TOLERANCE_D.get());
-=======
-    getController().setTolerance(BALANCE_TOLERANCE_P, BALANCE_TOLERANCE_D);
->>>>>>> 41436d7 (add auto balance)
-=======
-    getController().setTolerance(BALANCE_TOLERANCE_P.get(), BALANCE_TOLERANCE_D.get());
->>>>>>> 88faa89 (implemented tunable numbers)
-=======
-    getController().setTolerance(BALANCE_TOLERANCE_P.get(), BALANCE_TOLERANCE_D.get());
->>>>>>> fef06d234b1f4d0de6df11bb76090c722c603648
   }
 
   @Override
