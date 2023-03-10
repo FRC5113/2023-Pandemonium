9ae2062abebb0f9f6f8ab59482d6a053bbb7c5e9
Kevin Hwang <timmyhwang127@gmail.com>
2023-03-10 06:52:57 -0500

    added forward trajectory

--- src/main/java/com/frc5113/robot/RobotContainer.java
+++ src/main/java/com/frc5113/robot/RobotContainer.java
@@ -136,6 +136,13 @@ public class RobotContainer {
             // Pass config
             config);
 
+    Trajectory forwardTrajectory =
+        TrajectoryGenerator.generateTrajectory(
+            new Pose2d(0, 0, new Rotation2d(0)),
+            List.of(new Translation2d(2, 0), new Translation2d(2, 0)),
+            new Pose2d(4, 0, new Rotation2d(0)),
+            config);
+
     RamseteCommand ramseteCommand =
         new RamseteCommand(
             exampleTrajectory,
